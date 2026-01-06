890bf9691118c867a69d79dee590ad181505983d
Ritik Ranjan Pati <ritikranjanpati@gmail.com>
2026-01-06 12:33:15 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,6 +1,6 @@
-//const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
+const API_BASE_URL = "http://18.206.176.208";  // For reverse proxy it is mandatory so dont change
 
-const API_BASE_URL = "http://api.rittik.shop"
+//const API_BASE_URL = "http://api.rittik.shop"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 //const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
