7a7a34ecf7ea961bc4340c487e7566bba41e9ecb
Ritik Ranjan Pati <ritikranjanpati@gmail.com>
2026-01-09 13:26:27 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://18.206.176.208:84";  // For reverse proxy it is mandatory so dont change
+const API_BASE_URL = "http://a1bdf179db8404752b23ed39b7e27167-1149623347.us-east-1.elb.amazonaws.com";  // For reverse proxy it is mandatory so dont change
 
 //const API_BASE_URL = "http://api.rittik.shop"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
